0039d0d3c2b3792c169b496c03eabf2cf3c95dc5
joaoderocha <jharocha@gmail.com>
2021-02-23 02:38:43 -0300

    fix: remove promise

--- headless/redis/services/pub-sub/publish.js
+++ headless/redis/services/pub-sub/publish.js
@@ -3,11 +3,5 @@
 const { redisClient } = require('../../connection')
 
 exports.publish = async (channel, message) => {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(redisClient.publish(channel, JSON.stringify(message)))
-    } catch (error) {
-      reject(error)
-    }
-  })
+      redisClient.publish(channel, JSON.stringify(message));
 }
