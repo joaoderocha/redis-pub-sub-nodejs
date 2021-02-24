1c57b6dea6f62e43790ddd348c5358bbb5096cf6
joaoderocha <jharocha@gmail.com>
2021-02-24 04:35:28 -0300

    feat: inclui modulo do banco de dados

---
+++ headless/database/results-document.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { getDB } = require('./connection');
+
+module.exports = {
+  cadastrar(dadosReduce){
+    return getDB().collection('ReduceStep').insertOne(dadosReduce).then(({ops: [doc]}) => doc);
+  },
+  find(query, {limit=0, skip=0, ...options} = {}){
+    return getDB().collection('ReduceStep').find(query, options).limit(limit).skip(skip).toArray();
+  },
+  update(query, data){
+    return getDB().collection('ReduceStep').findOneAndUpdate(query, { $set: data }, { returnOriginal: false })
+    .then(({ value: doc }) => doc);
+  }
+};
